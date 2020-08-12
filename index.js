0e1831ef4970626e4cfe0a7f9911e7d408e7e097
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-08-12 12:36:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,9 +14,11 @@ class Formatter {
    for(let i=0;i<newarr.length;i++){
      if(newarr[i]===newarr[0]){
        str+= newarr[i].replace(newarr[0][0], newarr[0][0].toUpperCase());
+       str+=" "
      }
      else if(newarr[i]!=="the"||newarr[i]!=="a"||newarr[i]!=="an"||newarr[i]!=="but"||newarr[i]!=="of"||newarr[i]!=="and"||newarr[i]!=="for"||newarr[i]!=="at"||newarr[i]!=="by"||newarr[i]!=="and"||newarr[i]!=="from"){
-       str+= newarr[i].replace(newarr[i], newarr[i].toUpperCase());
+       str+= newarr[i].replace(newarr[i][0], newarr[i][0].toUpperCase());
+       str+=" "
      }
    }
    return str;
