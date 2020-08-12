5690764480d68b8f7b31dc5c470e854a30ddfefa
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-08-12 11:57:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return str.replace(str[0], str[0].toUpperCase())
+  }
+  static sanitize(str){
+   var newStr=str.replace(/[^\w\s'-]|_/g,"");
+   return newStr;
+  }
+  static titleize(str){
+   const arr=["the","a","an","but","of","and","for","at","by","and","from"];
+   const newarr=str.split(" ");
+   for(let i=0;i<newarr.length;i++){
+     if(newarr[0]===newarr[i]||newarr[i]!=for(let j=0;j<arr.length;j++)
+     {return arr[i];
+       
+     }){
+       
+       
+     }
+   }
+
+  }
 }
+Formatter.capitalize("sss");
