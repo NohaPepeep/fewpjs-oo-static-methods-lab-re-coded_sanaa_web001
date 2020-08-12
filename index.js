e29fb7c5de5207cd7f3c638e6ac5abf8cd09a202
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-08-12 12:11:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ class Formatter {
    const newarr=str.split(" ");
    for(let i=0;i<newarr.length;i++){
      if(newarr[i]===newarr[0]){
-        newarr[0].toUpperCase();
+       newarr[0]= newarr[0].toUpperCase();
      }
      for(let j=0;j<arr.length;j++){
        if(newarr[i]!==arr[j]){
-          newarr[i].replace(newarr[i][0],newarr[i][0].toUpperCase());
+         newarr[i]= newarr[i].replace(newarr[i][0],newarr[i][0].toUpperCase());
        }
      }
    }
